75c5b06d8cb52416ef49f02765333f48b47f555e
Isaac Helbling <imhelbling@gmail.com>
2016-10-25 22:17:04 -0400

    moved a bunch of stuff into the lib directory

--- src/rot-game/game.js
+++ src/rot-game/game.js
@@ -1,8 +1,8 @@
-import { ROT } from './vendor/rot'
-import { Systems } from './systems'
-import { FileManager } from './file-manager'
-import { SceneBuilder } from './scene-builder'
-import { View } from './view'
+import { ROT } from './lib/vendor/rot'
+import { Systems } from './lib/systems'
+import { FileManager } from './lib/file-manager'
+import { SceneBuilder } from './lib/scene-builder'
+import { View } from './lib/view'
 import jetpack from 'fs-jetpack'
 
 
@@ -10,14 +10,13 @@ export var Game = {
 
   init() {
     // setup game system
-    var scene     = new SceneBuilder('00').scene
+    var scene     = new SceneBuilder('06').scene
     var view      = new View(scene)
     var system    = new Systems(scene, view)
 
     // setup game engine
     var scheduler = new ROT.Scheduler.Simple()
-    var engine    = new ROT.Engine(scheduler)
-    system.engine = engine
+    system.engine = new ROT.Engine(scheduler)
 
     // starting up
     scheduler.add(system, true)
