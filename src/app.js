c2991d60b614c1b74d48896aec3e3014d89c0714
Isaac Helbling <imhelbling@gmail.com>
2016-11-08 01:37:59 -0500

    fixes #3 (somewhat jury-rigged)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import { Game } from './rot-game/game'
 
-document.addEventListener('DOMContentLoaded', function () {
-    Game.init()
-})
+// document.addEventListener('DOMContentLoaded', function () {
+//   Game.init()
+// })
+
+window.onload = ()=> {
+  setTimeout( ()=> {Game.init()}, 0)
+}
